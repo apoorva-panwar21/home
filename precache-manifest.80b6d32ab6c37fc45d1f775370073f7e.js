ae4386f7df8e9ca2cdf30c0152d3cac92c881f3e
apoorva-panwar21 <apoorva99.2101@gmail.com>
2020-10-07 17:22:21 +0530

    Updates

---
+++ precache-manifest.80b6d32ab6c37fc45d1f775370073f7e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7238e6bb99e5471c9ce8956c77e31d58",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "91700abe45e3d8231dc4",
+    "url": "/home/static/css/2.5b44f0a0.chunk.css"
+  },
+  {
+    "revision": "ebca8d6e00c0d5159643",
+    "url": "/home/static/css/main.e9084563.chunk.css"
+  },
+  {
+    "revision": "91700abe45e3d8231dc4",
+    "url": "/home/static/js/2.8fa335fd.chunk.js"
+  },
+  {
+    "revision": "80fa05aac9f80c71b5f99ecec6ead10d",
+    "url": "/home/static/js/2.8fa335fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ebca8d6e00c0d5159643",
+    "url": "/home/static/js/main.f3a6053e.chunk.js"
+  },
+  {
+    "revision": "5c07ecca0456e28e04f0",
+    "url": "/home/static/js/runtime-main.e084aacb.js"
+  },
+  {
+    "revision": "e2dfdb8e673e893201bc26d18433594e",
+    "url": "/home/static/media/resume.e2dfdb8e.pdf"
+  }
+]);
